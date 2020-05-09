eb3db3d53edd1a70000781d7fec75b1795c05e90
Dragos Manea <maneadr@gmail.com>
2020-05-09 21:44:40 +0300

    implement pop message in kv file

--- tds_calculator/common/pop_message.py
+++ tds_calculator/common/pop_message.py
@@ -7,20 +7,25 @@ from kivy.properties import ObjectProperty
 
 
 class PopMessage(Popup):
+    pop_label = ObjectProperty()
+
     def show_popup(self, value):
-        popmessage = Popup(
-            title_align="center",
-            title_size="18sp",
-            title_color=(0, 0, 0, 1),
-            title="Error Warning",
-            background="eta_calculator\\img\\grey.png",
-            content=Label(
-                font_size="18sp",
-                halign="center",
-                valign="middle",
-                color=(0, 0, 0, 1),
-                text="{}".format(value),
-            ),
-            size_hint=(0.7, 0.3),
-        )
+        popmessage = PopMessage()
+        popmessage.pop_label.text = f"{value}"
         popmessage.open()
+        # popmessage = Popup(
+        #     title_align="center",
+        #     title_size="18sp",
+        #     title_color=(0, 0, 0, 1),
+        #     title="Error Warning",
+        #     background="tds_calculator\\img\\grey.png",
+        #     content=Label(
+        #         font_size="18sp",
+        #         halign="center",
+        #         valign="middle",
+        #         color=(0, 0, 0, 1),
+        #         text="{}".format(value),
+        #     ),
+        #     size_hint=(0.7, 0.3),
+        # )
+        # popmessage.open()
